401b38512b5290f9ec671ee500915ab0380700f2
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2022-11-16 16:15:57 +0000

    setup: add minimal requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ['Click>=7.0', 'requests', 'snakedeploy', 'GitPython', 'peppy']
 
 test_requirements = [ ]
 
